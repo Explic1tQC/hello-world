3ac365016543107c8be3af5fc55d31222ab062c2
Explic1tQC <hugochiasson5@gmail.com>
2020-03-30 18:46:35 -0400

    Update

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ from tkinter import Button, Label, Tk
 fenetre = Tk()
 fenetre.title('Hello World!')
 
-mon_label = Label(test='Je suis du texte')
+mon_label = Label(text='Je suis du texte')
 mon_label.grid()
 
 mon_label2 = Label(text='Je suis aussi du texte')
